44014513bcbeede878827a37930babca12308b88
Niranjan Das <niranjandas.dev@gmail.com>
2025-12-10 21:06:48 +0530

    feat: initialize Express JWT authentication REST API

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import connectDB from "./config/db.js"
+
+dotenv.config()
+
+const app = express()
+
+const PORT  = process.env.PORT || 3000;
+
+app.use(express.json({limit:"30mb",extented: true}));
+app.use(express.urlencoded({limit:"30mb",extended:true}))
+
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("dev"))
+} else{
+    app.use(morgan("combined"))
+}
+
+app.use(cors())
+
+await connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
